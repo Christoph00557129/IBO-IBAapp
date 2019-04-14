09e9e7675687e39b381f2c982f4f77ea93c41ece
“Chris <hh0975832701@gmail.com>
2019-04-14 12:16:22 +0800

    Initial Commit

---
+++ IBO&IBAapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBO&IBAapp
+//
+//  Created by User24 on 2019/4/14.
+//  Copyright © 2019 User24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

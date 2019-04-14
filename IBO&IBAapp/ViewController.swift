91e8ffaa857060629129b54957fa527a3cf00e58
“Chris <hh0975832701@gmail.com>
2019-04-14 19:09:36 +0800

    first sub

--- IBO&IBAapp/ViewController.swift
+++ IBO&IBAapp/ViewController.swift
@@ -10,11 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var Birthdate: UIDatePicker!
+    @IBOutlet weak var singleConsider: UISwitch!
+    @IBOutlet weak var weightLable: UILabel!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var showLuck: UITextView!
+    
+    @IBOutlet weak var pic: UIImageView!
+    
+    @IBOutlet weak var weight: UISlider!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    
+    @IBAction func weightSlider(_ sender: Any) {
+        weightLable.text = "\(Int(weight.value))"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        var luckNum = Int.random(in: 0...2)
+        
+        if gender.selectedSegmentIndex == 0 {
+            luckNum = luckNum + 1
+        }
+        
+        let weightLuckNum = Int(weight.value) % 3
+        
+        if weightLuckNum == 0 {
+            luckNum = luckNum + 1
+        }else if weightLuckNum == 1{
+            luckNum = luckNum + 2
+        }else{
+        }
+        
+        if singleConsider.isOn{
+            luckNum = luckNum + 1
+        }
+        
+        let LuckNumFinal = luckNum % 3
+        
+        if nameText != nil &&
+            LuckNumFinal == 0{
+            pic.image=UIImage(named:"大窮")
+            showLuck.text = "\("衰！")"
+        }
+        else if nameText != nil &&
+        LuckNumFinal == 1{
+            pic.image=UIImage(named:"金貝貝")
+            showLuck.text = "\("吉！")"
+        }
+        else if nameText != nil{
+            pic.image=UIImage(named:"錢夫人")
+            showLuck.text = "\("大吉！")"
+        }else{
+            
+        }
+    }
+    
+    @IBAction func dismissKetboard(_ sender: Any) {
+    }
+    
+    
+    @IBAction func hideKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
 }
 
